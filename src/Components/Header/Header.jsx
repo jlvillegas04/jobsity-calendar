17a53c40122f129f8d8a5de7fd9f27d3d6aefcba
Juan Villegas <juanvj0425@gmail.com>
2023-10-31 16:40:09 -0500

    add: header styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,23 @@
+import "./Header.scss";
+
+import { BsFillCalendarEventFill } from "react-icons/bs";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+
 export default function Header() {
   return (
-    <div></div>
+    <header className="header">
+      <BsFillCalendarEventFill className="header-logo"/>
+      <h1 className="header-name">Jobsity Calendar</h1>
+      <button className="header-today-btn">
+        Today
+      </button>
+      <button className="chevron-btn">
+        <AiOutlineLeft />
+      </button>
+      <button className="chevron-btn">
+        <AiOutlineRight />
+      </button>
+    </header>
   )
 }
