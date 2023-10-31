be81b402a3b56cc360f22a944413a11859b1c28d
Juan Villegas <juanvj0425@gmail.com>
2023-10-30 19:40:34 -0500

    feat: basic styling and fonts import

--- src/Components/Day/Day.jsx
+++ src/Components/Day/Day.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
+import dayjs from 'dayjs'
 import './Day.scss'
 
 export default function Day({ day, rowIdx }) {
+  function getCurDay() {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "current-day" : "";
+  }
   return (
     <div className='days'>
       <header className="days-header">
         {rowIdx === 0 && (
           <p className="days-text-small">{day.format('ddd').toUpperCase()}</p>
         )}
-        <p className="days-text">
+        <p className={`days-text ${getCurDay()}`}>
           {day.format('DD')}
         </p>
       </header>
